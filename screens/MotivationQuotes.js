eb6fd67ec56636783f180b883dffec60b1b03a99
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-09-25 17:46:28 +1000

    Function to display content added

--- screens/MotivationQuotes.js
+++ screens/MotivationQuotes.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+
+export default function MotivationQuotes({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>“Your limitation—it’s only your imagination.”</Text>
+      <Button title="Back to Home" onPress={() => navigation.navigate('IntroScreen')} />
+    </View>
+  );
+}
