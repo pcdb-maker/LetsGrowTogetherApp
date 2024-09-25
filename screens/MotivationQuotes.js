26baabf48bd557e226285bd6e583075a80110de3
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-09-25 17:46:42 +1000

    Function to display inline styling added

--- screens/MotivationQuotes.js
+++ screens/MotivationQuotes.js
@@ -9,3 +9,8 @@ export default function MotivationQuotes({ navigation }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  text: { fontSize: 20, padding: 20, textAlign: 'center' },
+});
