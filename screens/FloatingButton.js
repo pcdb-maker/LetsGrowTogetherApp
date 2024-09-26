b8c6faeae1495e12f6145425e2e0d4deb9202112
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-09-26 13:34:07 +1000

    return

--- screens/FloatingButton.js
+++ screens/FloatingButton.js
@@ -2,3 +2,10 @@ import React from 'react';
 import { TouchableOpacity, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
+export default function FloatingButton({ onPress }) {
+  return (
+    <TouchableOpacity style={styles.floatingButton} onPress={onPress}>
+      <Icon name="accessibility" size={24} color="#ffffff" />
+    </TouchableOpacity>
+  );
+}
