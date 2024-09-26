a91733eec6550385d35541c4181c00d1856612e6
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-09-26 13:36:36 +1000

    Removed unrequired imports

--- screens/IntroScreen.js
+++ screens/IntroScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons'; 
 
 export default function IntroScreen({ navigation }) {
   return (
