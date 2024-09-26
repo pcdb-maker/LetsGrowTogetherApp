8125a270ad51bde7fa9f6f7e217aea7899330167
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-09-26 13:39:54 +1000

    Create TitleScreen.js

---
+++ screens/TitleScreen.js
@@ -0,0 +1,3 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Animated } from 'react-native';
+import FloatingButton from './FloatingButton'; // Import FloatingButton
