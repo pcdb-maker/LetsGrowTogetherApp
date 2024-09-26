804f6bf9fe84a8340507092ca5a58cbf1f62ae7b
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-09-26 13:33:02 +1000

    imports

---
+++ screens/AccessibilitySettings.js
@@ -0,0 +1,4 @@
+import React, { useState } from 'react';
+import { View, Text, Switch, TouchableOpacity, StyleSheet } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
