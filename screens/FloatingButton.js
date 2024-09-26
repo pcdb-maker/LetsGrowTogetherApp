4cf95d5112dcf2bc04e7c97a0b5c34b0b02abc80
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-09-26 13:34:12 +1000

    style

--- screens/FloatingButton.js
+++ screens/FloatingButton.js
@@ -9,3 +9,23 @@ export default function FloatingButton({ onPress }) {
     </TouchableOpacity>
   );
 }
+
+const styles = StyleSheet.create({
+  floatingButton: {
+    position: 'absolute',
+    top: 30,
+    left: 20,
+    backgroundColor: 'black',
+    borderRadius: 30,
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5, // Ensure elevation for Android
+    zIndex: 1000, // Ensure it's on top for iOS
+  },
+});
