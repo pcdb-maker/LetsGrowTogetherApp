d5c2e0fa69a3146765545485e0f3dbde18daa8af
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-09-26 13:40:00 +1000

    Update TitleScreen.js

--- screens/TitleScreen.js
+++ screens/TitleScreen.js
@@ -1,3 +1,8 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Animated } from 'react-native';
 import FloatingButton from './FloatingButton'; // Import FloatingButton
+
+export default function TitleScreen({ navigation }) {
+  const [showAnimation, setShowAnimation] = useState(false);
+  const fadeAnim = new Animated.Value(0); // Initial opacity value
+  const scaleAnim = new Animated.Value(0.5); // Initial scale value
