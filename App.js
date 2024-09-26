d64dbabcf06cfa12a68936d2e64b95f7fe6c8324
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-09-26 13:32:27 +1000

    Json file updates

--- App.js
+++ App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import TitleScreen from './screens/TitleScreen';
 import IntroScreen from './screens/IntroScreen';
 import WaterReminder from './screens/WaterReminder';
 import HabitTracker from './screens/HabitTracker';
-import MotivationQuotes from './screens/MotivationQuotes';
+import SensoryBreak from './screens/SensoryBreak';
+import PlantCondition from './screens/PlantCondition';
+import AccessibilitySettings from './screens/AccessibilitySettings';
 
 const Stack = createNativeStackNavigator();
-
-console.log("I am working")
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="IntroScreen">
+      <Stack.Navigator initialRouteName="TitleScreen" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="TitleScreen" component={TitleScreen} />
         <Stack.Screen name="IntroScreen" component={IntroScreen} />
         <Stack.Screen name="WaterReminder" component={WaterReminder} />
         <Stack.Screen name="HabitTracker" component={HabitTracker} />
-        <Stack.Screen name="MotivationQuotes" component={MotivationQuotes} />
+        <Stack.Screen name="SensoryBreak" component={SensoryBreak} />
+        <Stack.Screen name="PlantCondition" component={PlantCondition} />
+        <Stack.Screen name="AccessibilitySettings" component={AccessibilitySettings} />
       </Stack.Navigator>
     </NavigationContainer>
   );
