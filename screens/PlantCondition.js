9933559e7ba71adf7a4b959d00cbfa4471f3746c
Chloe O <66051807+pcdb-maker@users.noreply.github.com>
2024-09-26 13:37:24 +1000

    imports for plant condition

---
+++ screens/PlantCondition.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import seedImage from '../images/seed.png';
+import sproutImage from '../images/sprout.png';
+import smallPlantImage from '../images/small-plant.png';
+import grownPlantImage from '../images/grown-plant.png';
+
